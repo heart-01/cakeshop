4f13f088bed86c4063a0200bc0d642efd2815f57
siwat jomewattana <s1104300051612@gmail.com>
2019-03-29 16:10:57 +0700

    เชื่อมต่อกับ api  ที่เราสร้างขึ้น

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  customer : any=0;
+  constructor(public navCtrl: NavController, public navParam : NavParams, public http: Http) {
 
-  constructor(public navCtrl: NavController) {
-
+    this.http.get('http://localhost:8080/ShowCustomer')
+    .map(res=>res.json())
+    .subscribe(data=>{
+      this.customer=data;
+    });
+    
   }
 
 }
